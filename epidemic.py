More fixes to epidemic.py. Fixed log-likelihood calculation for full-length probability, input_y in main.py.

epidemic.py
@@ -31,23 +31,24 @@ class Epidemic():
 		# Update time remaining.
 		self.timeRemaining -= 1
 		# Copy hostGrid, in preparation for modifying it.
-		newHostGrid = hostGrid[:]
+		newHostGrid = self.hostGrid[:]
 		# Apply effect of action.
 		# Whatever the hostGrid of the selected host was, set it to R#
 		try:
 			newHostGrid[action] = SIR_R
-		except KeyError:
+		except IndexError:
+			print("Got action ",action,", but there are only ",self.nHosts," hosts.")
 			# Action doesn't correspond to a host - do nothing.
-			pass
+			raise IndexError
 		# Update hostState according to epidemic process.
-		for host in range(nHosts):
+		for host in range(self.nHosts):
 			# S hosts can become C
 			if newHostGrid[host] == SIR_S:
 				# Primary infection
 				if random.random() < self.epsilon:
 					newHostGrid[host] = SIR_C
 				# Secondary infection
-				if getNumInfectedNeighbours(newHostGrid, host) > 0:
+				if self.getNumInfectedNeighbours(host) > 0:
 					if random.random() < self.beta:
 						newHostGrid[host] = SIR_C
 			# C hosts can become I
@@ -63,9 +64,9 @@ class Epidemic():
 		if done:
 			# Reward based on number of S hosts.
 			if self.rewardForAnyNonI:
-				reward = sum(np.array(newHostGrid) != SIR_I)/nHosts # Higher reward for more S hosts
+				reward = sum(np.array(newHostGrid) != SIR_I)/self.nHosts # Higher reward for more S hosts
 			else:
-				reward = sum(np.array(newHostGrid) == SIR_S)/nHosts # Higher reward for more S hosts
+				reward = sum(np.array(newHostGrid) == SIR_S)/self.nHosts # Higher reward for more S hosts
 		else:
 			reward = 0
 		info = None

main.py
@@ -16,10 +16,9 @@ def getAction(tfprob, env):
 	if env == 'cartpole':
 		action = 1 if (y == np.array([0,1])).all() else 0
 	elif env == 'epidemic':
-		try:
-			action = np.asscalar(np.where(y == 1)[0][0])
-		except IndexError: # If y was all zeroes.
-			action = len(y)
+		print("y: ",y)
+		action = np.asscalar(np.where(y == 1)[0])
+		print("action: ",action)
 	return action, y
 
 def main(argv):
@@ -80,7 +79,11 @@ def main(argv):
 	# The loss function. This sends the weights in the direction of making actions
 	# that gave good advantage (reward over time) more likely, and actions that didn't less likely.
 	# Modified version of original; this one has high likelihood when input_y and probability match up.
-	loglik = tf.log(input_y*probability + (1 - input_y)*(1 - probability))
+	# For example, if input_y is [0, 1] and probability is [.2, .8], we should get
+	# tf.log()
+	# For example, if input_y is [0, 1, 0] and probability is [.1, .8, .1], we should get
+	# tf.log()
+	loglik = tf.log(tf.reduce_sum(tf.mul(input_y, probability)))
 	loss = -tf.reduce_mean(loglik * advantages)
 	newGrads = tf.gradients(loss,tvars)
 