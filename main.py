No adjustment for max in softmax. Fixed problem whereby probs would display as 1 always. Renamed probsBeforeSoftmax as logits.

main.py
@@ -84,6 +84,15 @@ def main(argv):
 		sess.run(init)
 
 		observation = env.reset() # Obtain an initial observation of the environment
+		x = np.reshape(observation,[1,D])
+
+		if verbose:
+			logits = sess.run(probsBeforeSoftmax,
+							feed_dict={observations_placeholder: x})
+			print("\n============= |BEFORE TRAINING ============")
+			print("Example observation: ", x)
+			print("Logits before softmax were: ", logits)
+			print("Probabilities for this were: ", softmax(logits[0]))
 
 		while episode_number < total_episodes:
 			episode_number += 1
@@ -93,11 +102,11 @@ def main(argv):
 
 			# Run the policy network and get an action to take.
 			# Purpose of action is soley to go into env.step().
-			tfprobsBeforeSoftmax = sess.run(probsBeforeSoftmax,
+			logits = sess.run(probsBeforeSoftmax,
 							feed_dict={observations_placeholder: x})
 			this_val_from_network = sess.run(estimated_value,
 							feed_dict={observations_placeholder: x})
-			action, y = getAction(tfprobsBeforeSoftmax)
+			action, y = getAction(logits)
 
 			xs.append(x) # observation
 			ys.append(action)
@@ -141,12 +150,13 @@ def main(argv):
 					if verbose:
 						print("\n============= END OF BATCH ============")
 						print("Last observation: ", x)
-						print("Probabilities for this were: ", softmax(tfprobsBeforeSoftmax))
+						print("Logits before softmax were: ", logits)
+						print("Probabilities for this were: ", softmax(logits[0]))
 						print("Estimated value was: ", this_val_from_network[0][0])
 						# If next couple of lines are run, learning doesn't happen.
-						tfprobBeforeSoftmaxWouldBe = sess.run(probsBeforeSoftmax,
+						logitsWouldBe = sess.run(probsBeforeSoftmax,
 							feed_dict={observations_placeholder: x})
-						print("Now probabilities would be: ", softmax(tfprobBeforeSoftmaxWouldBe))
+						print("Now probabilities would be: ", softmax(logitsWouldBe[0]))
 					# Reset the arrays.
 					xs, ys = [],[] # reset array memory
 					rewards, vals_from_network = [], []

policyNetwork.py
@@ -18,12 +18,12 @@ def inference(observation, nActions, H): # 'Inference' in the sense of 'predicti
 		# Length of action options is same as length of observation, plus 1 for no action..
 		W.append(tf.get_variable("W"+str(len(H)), shape=[H[-1], nActions],
 					 initializer=tf.contrib.layers.xavier_initializer()))
-		probsBeforeSoftmax = tf.matmul(layers[-1],W[-1])
-	return probsBeforeSoftmax
+		logits = tf.matmul(layers[-1],W[-1])
+	return logits
 
-def loss(probsBeforeSoftmax, input_y, advantages):
+def loss(logits, input_y, advantages):
 	#From here we define the parts of the network needed for learning a good policy.
-	cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=input_y, logits=probsBeforeSoftmax)
+	cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=input_y, logits=logits)
 	loglik = tf.log(tf.reduce_sum(cross_entropy))
 	loss = -tf.reduce_mean(loglik * advantages)
 	return loss

utils.py
@@ -11,16 +11,16 @@ def discount_rewards(r, gamma):
 	return discounted_r
 
 def softmax(x):
-	exps = np.exp(x - np.max(x))
+	exps = np.exp(x)
 	return exps / exps.sum(axis=0)
 
-def getAction(tfprobsBeforeSoftmax):
+def getAction(logits):
 	# y is
 	# tfprob and y should match up.
 	# tfprob [1.] should give action = 0, y = [1]
 	# tfprob [0.] should give action = 1, y = [0]
 	# TODO Change from np.random.multinomial, which is very slow.
-	pvals = softmax(tfprobsBeforeSoftmax[0])
+	pvals = softmax(logits[0])
 	try:
 		y = np.random.multinomial(n=1, pvals=pvals)
 	except ValueError as e: